e0aa0ef377ed8bf0f34b9e9b4082315fae5d2624
jonathan <jonathan@MacBook-Air-de-agilience.local>
2016-02-12 00:14:23 +0100

    update document feature

--- document.js
+++ document.js
@@ -2,7 +2,7 @@ var mongodb = require('mongodb');
 var auth = require('./auth.js');
 var config = require('./config.js');
 
-var Nightmare = require('nightmare');
+var phantom = require('phantom');
 var franc = require('franc');
 
 var request = require("request");
@@ -11,7 +11,7 @@ var cheerio = require("cheerio");
 var MongoClient = require('mongodb').MongoClient;
 var db;
 
-
+var phantomTimer = 2000;
 
 var tryMiningDocuments = function(){
   try{
@@ -30,6 +30,11 @@ var tryMiningDocuments = function(){
 }
 
 tryMiningDocuments();
+
+
+var callbackPageResults = function(results){
+
+}
 
 var miningDocuments = function(){
   console.log("request");
@@ -53,95 +58,110 @@ var miningDocuments = function(){
           (function(doc_index, docu){
             console.log("scrap url ", documents[doc_index], doc._id);
             try {
-              var nightmare = new Nightmare({waitTimeout:10000})
-                .useragent("Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36")
-                .goto(documents[doc_index])
-                .wait(2000)
-                // .screenshot('bandcamp2.png')
-                .evaluate(function () {
-                  return {
-                    html: document.documentElement.innerHTML,
-                    title: document.title,
-                    url : window.location.href
-                  }
-                })
-                .run(function (err, nightmare) {
-                  if (err) return console.log(err);
-                  var html = nightmare.html;
-                  var title = nightmare.title;
-                  var url = nightmare.url;
-                  var $ = cheerio.load(html); //use cheerio for jqeury in node
-                  var text = $('body').text(),
-                    nbWord = 0;
+              phantom.create(function (ph) {
+                ph.createPage(function (page) {
+                  page.set('settings.userAgent', 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.120 Safari/537.36');
+                  page.open(documents[doc_index], function (status) {
+                    console.log("URL : ", documents[doc_index],  status);
+                    if (status !== 'success') {
+                      console.error("phantom: error opening " + documents[doc_index], status);
+                      ph.exit();
+                    } else {
+                      // timeOut
+                      global.setTimeout(function () {
+                          page.evaluate(function () {
+                            return {
+                              html: document.documentElement.innerHTML,
+                              title: document.title,
+                              url : window.location.href
+                            }
+                          }, function (result) {
+                              ph.exit(); // EXTREMLY IMPORTANT
 
-                  // replace all non alpha numeric values
-                  text = text.replace(/\s+/g, " ")
-                    .replace(/[^a-zA-Z ]/g, "")
-                    .toLowerCase();
 
-                  var isENG = false;
-                  var languages = franc.all(text);
 
-                  for(var i = 0, ll = languages.length; i < 3 && i < ll; i++){
-                    if(languages[i][0] === 'eng'){
-                      isENG = languages[i][0];
-                      break;
+                              var html = result.html;
+                              var title = result.title;
+                              var url = result.url;
+                              var $ = cheerio.load(html); //use cheerio for jqeury in node
+                              var text = $('body').text(),
+                                nbWord = 0;
+
+
+                            // console.log(arguments);
+
+                              // replace all non alpha numeric values
+                              text = text.replace(/\s+/g, " ")
+                                .replace(/[^a-zA-Z ]/g, "")
+                                .toLowerCase();
+
+                              var isENG = false;
+                              var languages = franc.all(text);
+
+                              for(var i = 0, ll = languages.length; i < 3 && i < ll; i++){
+                                if(languages[i][0] === 'eng'){
+                                  isENG = languages[i][0];
+                                  break;
+                                }
+                              }
+
+                              if(isENG){
+                                // Split on spaces for a list of all the words on that page and
+                                // loop through that list.
+                                text.split(" ").forEach(function (word) {
+                                  // We don't want to include very short or long words because they're
+                                  // probably bad data.
+                                  if (config.topics[word]) {
+                                    nbWord++;
+                                  }
+                                });
+
+                                db.collection('docs').insertOne({
+                                  id: doc.id,
+                                  url: documents[doc_index],
+                                  true_url: url,
+                                  title: title,
+                                  user_id: doc.user.id,
+                                  screen_name: doc.user.screen_name,
+                                  nbWord: nbWord,
+                                  lang: isENG,
+                                  crawled_at: Date.now()
+                                });
+
+                                db.collection('tweet').update(
+                                  {id: doc.id},
+                                  { $set: { "crawled" : true } },
+                                  {multi: true}
+                                );
+                              } else {
+                                db.collection('tweet').update(
+                                  {id: doc.id},
+                                  { $set: { "crawled" : true, "foreign": true } },
+                                  {multi: true}
+                                );
+                              }
+
+                              crawledUrls++;
+                              console.log(crawledUrls, l)
+                              if(crawledUrls === l){
+                                db.close();
+                                tryMiningDocuments();
+                              }
+
+
+
+
+
+
+                          });
+                      }, phantomTimer);
                     }
-                  }
-
-                  if(isENG){
-                    // Split on spaces for a list of all the words on that page and
-                    // loop through that list.
-                    text.split(" ").forEach(function (word) {
-                      // We don't want to include very short or long words because they're
-                      // probably bad data.
-                      if (config.topics[word]) {
-                        nbWord++;
-                      }
-                    });
-
-                    db.collection('docs').insertOne({
-                      id: doc.id,
-                      url: documents[doc_index],
-                      true_url: url,
-                      title: title,
-                      user_id: doc.user.id,
-                      screen_name: doc.user.screen_name,
-                      nbWord: nbWord,
-                      lang: isENG,
-                      crawled_at: Date.now()
-                    });
-
-                    db.collection('tweet').update(
-                      {id: doc.id},
-                      { $set: { "crawled" : true } },
-                      {multi: true}
-                    );
-                  } else {
-                    db.collection('tweet').update(
-                      {id: doc.id},
-                      { $set: { "crawled" : true, "foreign": true } },
-                      {multi: true}
-                    );
-                  }
-
-                  crawledUrls++;
-                  console.log(crawledUrls, l)
-                  if(crawledUrls === l){
-                    db.close();
-                    tryMiningDocuments();
-                  }
-                })
-                .end(function(){
-                  crawledUrls++;
-                  console.log(crawledUrls, l);
-                  // miningDocuments();
-
+                  });
                 });
-              } catch (error){
-                console.log(error);
-              }
-
+              });
+            } catch(err){
+              console.log(err);
+            }
           })(i, doc);
         }
       }
